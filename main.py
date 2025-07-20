dba3b52ebe1934827a36ce0b3a74e718b90d757c
annapeterson89@gmail.com <annapeterson89@gmail.com>
2025-07-20 12:41:38 -0400

    just a hello world to verify workflow

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-import sys
-from ai_reviewer.langchain_reviewer import LangChainReviewer
-
-def main():
-    diff = sys.stdin.read()
-    reviewer = LangChainReviewer()
-    review = reviewer.review(diff)
-    print(review)
-
-if __name__ == "__main__":
-    main()
+print("Hello, World!")
