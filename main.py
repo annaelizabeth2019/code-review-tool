34392c6bd8d627b99e1eb7880a0dbce59df6630a
annapeterson89@gmail.com <annapeterson89@gmail.com>
2025-07-20 12:45:59 -0400

    just hello world for now

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-import json, os
-
-with open(os.getenv("GITHUB_EVENT_PATH")) as f:
-    event_data = json.load(f)
-    print("PR title:", event_data["pull_request"]["title"])
+print("Hello, World!")
