ce0bd9ad87bdffb6187aefd4e62b8e68bd848840
annapeterson89@gmail.com <annapeterson89@gmail.com>
2025-07-20 12:45:01 -0400

    wrong file name

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hello, World!")
+import json, os
+
+with open(os.getenv("GITHUB_EVENT_PATH")) as f:
+    event_data = json.load(f)
+    print("PR title:", event_data["pull_request"]["title"])
